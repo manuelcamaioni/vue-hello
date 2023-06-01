12780386254486a9e826fb9f7de26da238c2a48a
manuelcamaioni <camaioni.manuel@gmail.com>
2023-06-01 18:08:54 +0200

    added image

---
+++ script.js
@@ -0,0 +1,10 @@
+const { createApp } = Vue;
+
+createApp({
+    data() {
+        return {
+            message: "NOOOOO Vue!",
+            image: "https://picsum.photos/200/300",
+        };
+    },
+}).mount("#app");
